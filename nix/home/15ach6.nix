f481df23b86ceee8fc96c6e327b4291dcef6c173
idf <ardek66@tutanota.com>
2025-11-24 17:57:35 +0200

    Add multimedia packages

--- nix/home/15ach6.nix
+++ nix/home/15ach6.nix
@@ -3,11 +3,19 @@
   imports = [./common.nix];
 
 
-  home.packages = [ pkgs.vice pkgs.goattracker pkgs.furnace pkgs.webcamoid pkgs.gimp pkgs.jamesdsp pkgs.openmw ]; 
+  home.packages = [ pkgs.vice pkgs.goattracker pkgs.furnace pkgs.gimp pkgs.jamesdsp pkgs.openmw pkgs.bottles ]; 
 
   programs.lan-mouse = {
     enable = true;
     systemd = true;
     settings.authorized_fingerprints."09:43:ab:24:7f:ae:2f:ef:f2:28:4e:fe:e5:c2:d5:b0:14:c2:7a:61:17:59:22:90:4e:a6:a6:f0:32:77:69:cf" = "lain-iwakura";
+  };
+
+  programs.kodi = {
+    enable = true;
+  };
+
+  programs.lutris = {
+    enable = true;
   };
 }
